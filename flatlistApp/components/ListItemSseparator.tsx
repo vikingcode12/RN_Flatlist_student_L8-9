78e8475cb97ab6d1a7a8b7bd4695a0ab2c279aee
Shannon M. Denna <sdenna@d211.org>
2025-01-27 12:50:45 -0600

    Added student starter code

---
+++ flatlistApp/components/ListItemSseparator.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, View, ViewStyle } from "react-native";
+import React from "react";
+import colors from "../styles/colors";
+
+const ListItemSseparator = () => {
+  return <View />;
+};
+
+export default ListItemSseparator;
+
+const styles = StyleSheet.create({
+  separator: {},
+});
